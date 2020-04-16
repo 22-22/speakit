92503463b601b9e04910bc49fadb55f4a73ccee8
22-22 <kogol.julia@gmail.com>
2020-04-16 23:03:58 +0300

    feat: update webpack config

--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -1,73 +1,33 @@
-const webpack = require('webpack');
 const path = require('path');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
-    const isProduction = options.mode === 'production';
+const isProduction = options.mode === 'production';
 
-    const config = {
-        mode: isProduction ? 'production' : 'development',
-        devtool: isProduction ? 'none' : 'source-map',
-        watch: !isProduction,
-        entry: ['./src/script.js', './src/style.css'],
-        //entry: './src/script.js',
-        output: {
-            path: path.join(__dirname, '/dist'),
-            filename: 'main.js',
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                      loader: 'babel-loader',
-                      options: {
-                        presets: ['@babel/preset-env']
-                      }
-                    }
-                }, {
-                    test: /\.scss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' 
-                    ]
-                }, {
-                    test: /\.(png|svg|jpe?g|gif)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                        }
-                    ]
-                }, {
-                    test: /\.html$/,
-                    loader: 'html-loader',
-                },
-                {
-                    test: /\.css$/i,
-                   // use: ['style-loader', 'css-loader'],
-                   use: ['css-loader'],
-                },
-            ]
-        },
-
-        plugins: [
-          //   new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: 'index.html'
-            }),
-            new MiniCssExtractPlugin({
-                filename: 'style.css'
-            }),
-            new CopyWebpackPlugin([
-                {from: './src/assets/img', to: './img'},
-              ]),
-        ]
-    }
-
-    return config;
+const conf = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
+    entry: './src/script.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        overlay: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin([
+            { from: './src/style', to: './style' },
+            { from: './src/assets', to: './assets' },
+        ]),
+    ]
+}
+return conf;
 }
 
+// module.exports = conf;
