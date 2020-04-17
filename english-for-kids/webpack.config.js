ee65449e9a01b530a50334f2f0e74260b4c82c1f
22-22 <kogol.julia@gmail.com>
2020-04-17 21:00:03 +0300

    feat: add babel-loader

--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -17,6 +17,22 @@ const conf = {
     devServer: {
         overlay: true
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }, 
+        ]
+    },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html'
