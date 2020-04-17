d1f9d9f7c7a11c7065b69b17e2126b74c228976a
22-22 <kogol.julia@gmail.com>
2020-04-17 13:46:44 +0300

    feat: add style changes on pressing switcher

--- english-for-kids/src/script.js
+++ english-for-kids/src/script.js
@@ -10,7 +10,7 @@ let categoryPage = document.querySelector('#category')
 const pageContainer = document.querySelector('#pageContainer');
 const mainCard = document.querySelectorAll('.main-card');
 const burger = document.querySelector('.burger');
-// let cardElement;
+const switcher = document.querySelector('.switcher');
 
 // remove templates from DOM
 // pageContainer.removeChild(mainPage);
@@ -36,7 +36,7 @@ function generateCard(categoryCount) {
   <div class="card__rotate"></div>
 </div>`;
     cardElement.addEventListener('click', (e) => {
-      if (!e.target.classList.contains('card__rotate')) {
+      if (!e.target.classList.contains('card__rotate') && switcher.checked === false) {
         let sound = new Audio(el.audioSrc);
         sound.play();
       }
@@ -46,16 +46,12 @@ function generateCard(categoryCount) {
   });
   // go back on main page we can only when click to menu
   categoryPage.insertAdjacentHTML('afterbegin', '<div class="rating"></div>')
-  categoryPage.insertAdjacentHTML('beforeend', '<div class="buttons"></div>')
+  categoryPage.insertAdjacentHTML('beforeend', '<div class="btn-container"><button class="btn hidden">Start game</button></div>')
   pageContainer.append(categoryPage);
   return pageContainer;
 }
 
-
-
-
 mainPage.addEventListener('click', (event) => {
-
   if (event.target.tagName === 'IMG' || event.target.tagName === 'A') {
     pageContainer.removeChild(mainPage);
     let currentCategory = 0;
@@ -77,7 +73,6 @@ categoryPage.addEventListener('click', (event) => {
       })
     }
   });
-
 })
 
 categoryPage.addEventListener('mouseout', (event) => {
@@ -103,6 +98,8 @@ burger.addEventListener('click', (e) => {
 // menu links
 document.querySelector('.menu').addEventListener('click', (event) => {
   if (event.target.tagName === 'A') {
+    document.querySelectorAll('.menu__item').forEach(el => el.classList.remove('link__active'));
+    event.target.classList.add('link__active');
     if (pageContainer.children[0].id === 'main') {
       if (event.target.textContent === 'Main Page') {
 
@@ -147,13 +144,39 @@ document.querySelector('.menu').addEventListener('click', (event) => {
 //     document.querySelector('.menu').classList.remove('menu-active');
 // })
 
-document.querySelector('.switch').addEventListener('click', (e) => {
- 
-  mainCard.forEach(el => {
-    if (document.querySelector('.switch').checked === true) {
-      el.classList.add('red');
+switcher.addEventListener('click', (e) => {
+  if (pageContainer.children[0].id === 'main') {
+    mainCard.forEach(el => {
+      if (switcher.checked === true) {
+        el.classList.add('red');
+      } else {
+        el.classList.remove('red');
+      }
+    })
+  } else {
+    if (switcher.checked === true) {
+      document.querySelectorAll('.card__rotate').forEach(el => {
+        el.classList.add('hidden');
+      })
+      document.querySelectorAll('.card__word').forEach(el => {
+        el.classList.add('hidden');
+      })
+      document.querySelectorAll('.card__front').forEach(el => {
+        el.classList.add('card__cover');
+      document.querySelector('.btn').classList.remove('hidden');
+      })
     } else {
-      el.classList.remove('red');
+      document.querySelectorAll('.card__rotate').forEach(el => {
+        el.classList.remove('hidden');
+      })
+      document.querySelectorAll('.card__word').forEach(el => {
+        el.classList.remove('hidden');
+      })
+      document.querySelectorAll('.card__front').forEach(el => {
+        el.classList.remove('card__cover');
+      })
+      document.querySelector('.btn').classList.add('hidden');
     }
-  })
+  }
 })
+
