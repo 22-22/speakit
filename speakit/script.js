2b1f0d82964e9c56fafbb6b51fb1ed667df42f2c
22-22 <kogol.julia@gmail.com>
2020-04-25 15:17:22 +0300

    feat: add path generation to images and sound

--- speakit/script.js
+++ speakit/script.js
@@ -1,3 +1,5 @@
+let image = document.querySelector('.image')
+
 document.querySelector('.start-button').addEventListener('click', (e) => {
   document.querySelector('.start').classList.add('hidden');
   document.querySelector('.start').classList.remove('start');
@@ -14,6 +16,8 @@ async function getWords(page, group) {
     if (idx < 10) {
       let word = document.createElement('div');
       word.classList.add('item');
+      let id = item.image.substr(-11, 7);
+      word.setAttribute('id', id);
       word.insertAdjacentHTML('afterbegin', `<div class="item--transcript">${item.transcription}</div>`);
       word.insertAdjacentHTML('afterbegin', `<div class="item--word">${item.word}</div>`);
       word.insertAdjacentHTML('afterbegin', '<div class="item--icon"></div>');
@@ -21,6 +25,17 @@ async function getWords(page, group) {
     }
   })
 }
+
+document.querySelector('.items').addEventListener('click', (e) => {
+  let currentItem = e.target.closest('.item');
+  image.src = `https://raw.githubusercontent.com/22-22/rslang/rslang-data/data/files/${currentItem.id}.jpg`
+  let audio = new Audio(`https://raw.githubusercontent.com/22-22/rslang/rslang-data/data/files/${currentItem.id}.mp3`)
+  audio.play();
+  document.querySelectorAll('.item').forEach(el => {
+    el.classList.remove('item-active');
+  })
+  currentItem.classList.add('item-active');
+})
 
 function calculatePage() {
   let arr = [];
@@ -33,9 +48,16 @@ function calculatePage() {
 }
 
 document.querySelector('.navigation').addEventListener('click', (e) => {
+  document.querySelector('.items').innerHTML = '';
+
   let group = 0;
   let page = calculatePage();
-   
   getWords(page, group);
 
 })
+
+window.addEventListener('DOMContentLoaded', () => {
+  let group = 0;
+  let page = calculatePage();
+  getWords(page, group);
+});
