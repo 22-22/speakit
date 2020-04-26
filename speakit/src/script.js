cbca4d84e5a8626e8a2156029b9c091922fe7224
22-22 <kogol.julia@gmail.com>
2020-04-26 19:25:13 +0300

    feat: fetch translation for statistics

--- speakit/src/script.js
+++ speakit/src/script.js
@@ -30,27 +30,24 @@ function getWords(page, group) {
           word.classList.add('item');
           let id = item.image.substr(-11, 7);
           word.setAttribute('id', id);
-         // word.insertAdjacentHTML('afterbegin', `<div class="item--translation">${item.transcription}</div>`);
+          word.insertAdjacentHTML('afterbegin', `<div class="item--translation hidden"></div>`);
           word.insertAdjacentHTML('afterbegin', `<div class="item--transcript">${item.transcription}</div>`);
           word.insertAdjacentHTML('afterbegin', `<div class="item--word">${item.word}</div>`);
           word.insertAdjacentHTML('afterbegin', '<div class="item--icon"></div>');
           document.querySelector('.items').append(word);
           wordsOnPage.push(item.word);
           imagesOnPage.push(id);
+
+          const urlTr = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200425T122428Z.8ca2d87f5b800f7e.9a40ebb21a85ec1dd1d616f3bb88861b71b6b3b7&text= ${item.word} &lang=en-ru`;
+          fetch(urlTr)
+            .then(res => res.json())
+            .then(data => {
+              document.querySelectorAll('.item--translation')[idx].textContent = data.text;
+            });
         }
       })
     });
   return wordsOnPage;
-}
-
-function getTranslation(currentWord) {
-  const url = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200425T122428Z.8ca2d87f5b800f7e.9a40ebb21a85ec1dd1d616f3bb88861b71b6b3b7&text= ${currentWord} &lang=en-ru`;
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      document.querySelector('.translation-displayed').textContent = data.text;
-      console.log(wordsOnPage)
-    });
 }
 
 function onItemClick(e) {
@@ -62,11 +59,17 @@ function onItemClick(e) {
     el.classList.remove('item-active');
   })
   currentItem.classList.add('item-active');
-  let currentWord = currentItem.childNodes[1].textContent;
-  getTranslation(currentWord);
+  document.querySelector('.translation-displayed').textContent = currentItem.lastChild.textContent;
 }
 
 document.querySelector('.items').addEventListener('click', onItemClick);
+document.querySelector('.results-container').addEventListener('click', (e) => {
+  let currentItem = e.target.closest('.item');
+  image.src = `https://raw.githubusercontent.com/22-22/rslang/rslang-data/data/files/${currentItem.id}.jpg`
+  let audio = new Audio(`https://raw.githubusercontent.com/22-22/rslang/rslang-data/data/files/${currentItem.id}.mp3`)
+  audio.play();
+
+});
 
 // choose level
 document.querySelector('.nav').addEventListener('click', (e) => {
@@ -79,14 +82,39 @@ document.querySelector('.nav').addEventListener('click', (e) => {
   getWords(page, group);
 })
 
-function checkIfCorrectWord(recognizedWord) {
+function showResults(e) {
+  document.querySelector('#app').classList.add('hidden');
+  document.querySelector('.results').classList.remove('hidden');
+  document.querySelectorAll('.item--translation').forEach(el => {
+    el.classList.remove('hidden');
+  })
+
+  document.querySelectorAll('.item').forEach((el, idx, arr) => {
+    if (el.classList.contains('item-guessed')) {
+      let elCloneSuccess = el.cloneNode(true);
+      document.querySelector('.success-items').append(elCloneSuccess);
+      let successNumber = document.querySelector('.success-items').childElementCount;
+      document.querySelector('.success-number').textContent = successNumber;
+    } else {
+      let elCloneError = el.cloneNode(true);
+      document.querySelector('.error-items').append(elCloneError);
+      let errorsNumber = document.querySelector('.error-items').childElementCount;
+      document.querySelector('.errors-number').textContent = errorsNumber;
+    }
+  })
+}
+
+function checkIfCorrectWord(recognizedWordLowerCase) {
   wordsOnPage.forEach((el, idx) => {
-    if (el === recognizedWord) {
+    if (el.toLowerCase() === recognizedWordLowerCase) {
       image.src = `https://raw.githubusercontent.com/22-22/rslang/rslang-data/data/files/${imagesOnPage[idx]}.jpg`;
       document.getElementById(`${imagesOnPage[idx]}`).classList.add('item-guessed');
       document.querySelector('.rating').insertAdjacentHTML('beforeend', '<div class="star"></div>');
     }
   })
+  if (document.querySelectorAll('.item-guessed').length === 10) {
+    showResults(e);
+  }
 }
 
 // speech recognition start
@@ -109,8 +137,9 @@ recognition.lang = 'en-US';
 
 recognition.addEventListener('result', (e) => {
   let recognizedWord = e.results[0][0].transcript;
-  document.querySelector('.input-speech').textContent = recognizedWord;
-  checkIfCorrectWord(recognizedWord);
+  let recognizedWordLowerCase = recognizedWord.toLowerCase()
+  document.querySelector('.input-speech').textContent = recognizedWordLowerCase;
+  checkIfCorrectWord(recognizedWordLowerCase);
 })
 
 recognition.addEventListener('end', recognition.start)
@@ -118,6 +147,9 @@ recognition.addEventListener('end', recognition.start)
 function returnToRegularMode(e) {
   document.querySelectorAll('.item').forEach(el => {
     el.classList.remove('item-guessed');
+  })
+  document.querySelectorAll('.item--translation').forEach(el => {
+    el.classList.add('hidden');
   })
   document.querySelector('.rating').innerHTML = '';
   document.querySelector('.input-speech').classList.add('hidden');
@@ -138,22 +170,7 @@ document.querySelector('.btn-restart').addEventListener('click', (e) => {
 
 // results
 document.querySelector('.btn-results').addEventListener('click', (e) => {
-  document.querySelector('#app').classList.add('hidden');
-  document.querySelector('.results').classList.remove('hidden');
-
-  document.querySelectorAll('.item').forEach((el, idx, arr) => {
-    if (el.classList.contains('item-guessed')) {
-      let elCloneSuccess = el.cloneNode(true);
-      document.querySelector('.success-items').append(elCloneSuccess);
-      let successNumber = document.querySelector('.success-items').childElementCount;
-      document.querySelector('.success-number').textContent = successNumber;
-    } else {
-      let elCloneError = el.cloneNode(true);
-      document.querySelector('.error-items').append(elCloneError);
-      let errorsNumber = document.querySelector('.error-items').childElementCount;
-      document.querySelector('.errors-number').textContent = errorsNumber;
-    }
-  })
+  showResults(e);
 })
 
 // return button
@@ -162,6 +179,10 @@ document.querySelector('.btn-return').addEventListener('click', (e) => {
   document.querySelector('.success-items').innerHTML = '';
   document.querySelector('.error-items').innerHTML = '';
   document.querySelector('.results').classList.add('hidden');
+  document.querySelectorAll('.item--translation').forEach(el => {
+    el.classList.add('hidden');
+  })
+
 })
 
 //new game button
